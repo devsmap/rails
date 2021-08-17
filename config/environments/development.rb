793ce5e8c88c7159eb2668edbbfb91e9a49428fe
Alan Alves <alan86alves@gmail.com>
2021-08-17 18:32:06 -0300

    Adicionando hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  
+  config.hosts << "devsmap.local"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -33,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
-  config.action_mailer.default_url_options = { :host => "http://localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "http://devsmap.local" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
