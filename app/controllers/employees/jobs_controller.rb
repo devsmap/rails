94f35628f8d801941538b3390eec7e0d3622111b
Alan Alves <alan86alves@gmail.com>
2021-08-30 15:05:06 -0300

    Finalizado a edicao de vagas

--- app/controllers/employees/jobs_controller.rb
+++ app/controllers/employees/jobs_controller.rb
@@ -12,10 +12,23 @@ class Employees::JobsController < Employees::ApplicationController
   def edit
   end
 
+  def update
+    if @job.update(job_params)
+      redirect_to employees_jobs_path, notice: "Sucesso"
+    else
+      render action: 'edit'
+    end
+  end
+
+
   private
 
     def set_job
       @job = Job.find(params[:id])
+    end
+
+    def job_params
+      params.require(:job).permit(:category_id, :title, :company, :city_id, :link, :description)
     end
 
 end
