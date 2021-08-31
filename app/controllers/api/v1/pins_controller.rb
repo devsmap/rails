e92d3258025adfee05b9f242c49faa01aa381593
Alan Alves <alan86alves@gmail.com>
2021-08-31 12:04:00 -0300

    API pins default

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,18 +1,18 @@
 class Api::V1::PinsController < Api::V1::ApplicationController
   
   def default
-    pins_json = []
+    pins_default_json = []
 
     jobs = Job.select(" jobs.category_id
                        ,cities.latitude
                        ,cities.longitude
                        ,sum(1) as total")
               .joins(:city)
-              .where(category: 1)
+              .where(category: params[:category_id])
               .group('jobs.category_id, cities.id')
 
     jobs.each do |job|
-      pins_json << {
+      pins_default_json << {
         category_id: job.category_id,
         latitude: job.latitude,
         longitude: job.longitude,
@@ -21,7 +21,7 @@ class Api::V1::PinsController < Api::V1::ApplicationController
     end
 
     render json: {
-      data: pins_json
+      data: pins_default_json
     }
   end
 
